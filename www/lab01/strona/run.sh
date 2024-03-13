52ccb75bfbf90c7f1482377b843c7f2afeb262e8
Bogna Pawlus <bogna.pawlus@gmail.com>
2024-03-13 21:52:01 +0100

    some corrections

--- www/lab01/strona/run.sh
+++ www/lab01/strona/run.sh
@@ -1,4 +1,6 @@
 #https://docs.github.com/en/pages/setting-up-a-github-pages-site-with-jekyll/creating-a-github-pages-site-with-jekyll
+#jekyll new --skip-bundle .
+
 python3 program.py
 
 bundle install
